6f356edfe22aad884a0e6086b4b89c34dcea906d
Richard Ferrers <richard.ferrers@monash.edu>
2016-02-01 14:48:06 +1100

    Add title and explanation

--- testJavascript/interaction.js
+++ testJavascript/interaction.js
@@ -1,1 +1,30 @@
-console.log('hello world');
+//interact
+
+// get html
+
+var cat_img = document.getElementById('cat_img');
+
+var feed_me = document.getElementById('myButton');
+
+
+// event listener
+
+cat_img.addEventListener("click", meow);
+
+feed_me.addEventListener("click", yummy);
+
+
+
+function meow() {
+	alert("Meow");
+}
+
+
+function yummy() {
+	alert("Yummy");
+}
+
+// manipulate html element
+
+
+
